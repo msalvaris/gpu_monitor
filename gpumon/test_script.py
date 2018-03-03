065a0a41a689f605dd0aeef6c8f5669d828fa93c
MSalvaris <msalvaris@users.noreply.github.com>
2018-03-03 17:42:52 +0000

    Adds nvidia-ml-py3 to requirements Adds temporary test_script

---
+++ gpumon/test_script.py
@@ -0,0 +1,15 @@
+from pynvml import *
+
+
+def main():
+    nvmlInit()
+    print("Driver Version: {}").format(nvmlSystemGetDriverVersion())
+    deviceCount = nvmlDeviceGetCount()
+    for i in range(deviceCount):
+        handle = nvmlDeviceGetHandleByIndex(i)
+        print("Device {} : {}".format(i, nvmlDeviceGetName(handle)))
+
+    nvmlShutdown()
+
+if __name__=="__main__":
+    main()
