9316800044c96d44f58f50b59a3dd147479dee7a
MSalvaris <msalvaris@users.noreply.github.com>
2018-03-28 15:47:49 +0100

    Fixes import

--- gpumon/influxdb/influxdb_gpu_logger.py
+++ gpumon/influxdb/influxdb_gpu_logger.py
@@ -4,7 +4,7 @@ logging.basicConfig(level=logging.INFO)
 from time import sleep
 
 import fire
-from gpu_interface import start_record_gpu_to
+from gpumon.gpu_interface import start_record_gpu_to
 from influxdb import InfluxDBClient
 from toolz import curry, compose
 
